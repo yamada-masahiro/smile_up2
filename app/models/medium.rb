bd9e93822daddee0ad9be4baece1f0c3193597bf
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-12-27 06:15:25 +0900

    メディア名、振り仮名のバリデーションを行う

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,2 +1,9 @@
 class Medium < ApplicationRecord
+  before_save { self.furigana.gsub!(/\s+/, '') }
+  validates :name,        presence: true,
+                          length: { maximum: 100 }
+  validates :furigana,    presence: true,
+                          length: { maximum: 200 },
+                          format: { with: /\A\s*[ぁ-ろわをんゔー]+\s*\z/,
+                                    message: 'は、ひらがなで記してください' }
 end
