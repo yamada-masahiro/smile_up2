043f26760ef0e966309c08894166ec17663835f2
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-12-27 06:41:22 +0900

    `website_url` のバリデーションを行う

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -6,4 +6,9 @@ class Medium < ApplicationRecord
                           length: { maximum: 200 },
                           format: { with: /\A\s*[ぁ-ろわをんゔー]+\s*\z/,
                                     message: 'は、ひらがなで記してください' }
+  validates :website_url, allow_blank: true,
+                          length: { maximum: 200 },
+                          url: { schemes: %w(http https),
+                                 no_local: true,
+                                 public_suffix: true }
 end
