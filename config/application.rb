e8ec2162e622d6490d75ed72281aa8335e503d2d
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-12-30 17:05:32 +0900

    SMU-9 日本語化の設定を行う

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module SmileUp2
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
