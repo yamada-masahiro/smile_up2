ea08aebd94617a7caf15bb3ad5f622f0d3fb0cea
YAMADA Masahiro <yamada_masahiro@nifty.com>
2024-02-21 16:54:08 +0900

    `ng_lists#normal` コントローラを作る

--- app/controllers/ng_lists_controller.rb
+++ app/controllers/ng_lists_controller.rb
@@ -1,4 +1,16 @@
 class NgListsController < ApplicationController
   def normal
+    @desirable_reporters   = Reporter.includes(:medium)
+                               .where('desirability >= ?', Reporter.desirabilities[:somewhat])
+                               .order(`reporter.desirability DESC,
+                                       media.furigana        ASC,
+                                       media.id              ASC,
+                                       reporter.furigana     ASC`)
+    @undesirable_reporters = Reporter.includes(:medium)
+                               .where('desirability <= ?', Reporter.desirabilities[:not_very])
+                               .order(`reporter.desirability ASC,
+                                       media.furigana        ASC,
+                                       media.id              ASC,
+                                       reporter.furigana     ASC`)
   end
 end
